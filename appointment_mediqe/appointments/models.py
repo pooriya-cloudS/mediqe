2427b66df3ad3232119283c37fdb1a629f268eb8
AmirMosafery <amirm4a112@gmail.com>
2025-07-24 03:18:39 -0700

    create Schedule and Appointment model

--- appointment_mediqe/appointments/models.py
+++ appointment_mediqe/appointments/models.py
@@ -1,3 +1,32 @@
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Schedule(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    #doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    weekday = models.IntegerField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    location = models.CharField(max_length=100)
+    is_active = models.BooleanField(default=False)
+class Appointment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    STATUS_CHOICES = [
+        ('Pending', 'Pending'),
+        ('Confirmed', 'Confirmed'),
+        ('Cancelled', 'Cancelled'),
+        ('Completed', 'Completed'),
+        ('NoShow', 'NoShow'),
+    ]
+    #doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    #patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE,blank=True, null=True)
+    appointment_time = models.TimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    cancelled_at =models.DateTimeField(auto_now=True)
+    notes = models.TextField(blank=True)

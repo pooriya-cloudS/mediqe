74c2b9aa8ec03afe90f3693a2367f080a50f28ad
AmirMosafery <amirm4a112@gmail.com>
2025-07-24 03:57:02 -0700

    create prescription and  medicalfile models

--- appointment_mediqe/medical_records/models.py
+++ appointment_mediqe/medical_records/models.py
@@ -12,11 +12,40 @@ class MedicalRecord(models.Model):
         ('Closed', 'Closed'),
         ('Archived', 'Archived'),
     ]
-    patient = models.ForeignKey(User, on_delete=models.CASCADE)
-    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+   # patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    #doctor = models.ForeignKey(User, on_delete=models.CASCADE)
     visit_reason = models.TextField(blank=True, null=True)
     diagnosis = models.TextField(blank=True, null=True)
     status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Open')
     is_sensitive = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+class Prescription(models.Model):
+    STATUS_CHOICES = [
+        ('Active', 'Active'),
+        ('Completed', 'Completed'),
+        ('Cancelled', 'Cancelled'),
+    ]
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    record = models.ForeignKey(MedicalRecord, on_delete=models.CASCADE, related_name='prescriptions')
+    medication = models.CharField(max_length=100)
+    dosage = models.CharField(max_length=100)
+    instructions = models.TextField()
+    start_date = models.DateField()
+    end_date = models.DateField()
+    renewable = models.BooleanField(default=False)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Active')
+
+
+class MedicalFile(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    record = models.ForeignKey(MedicalRecord, on_delete=models.CASCADE, related_name='files')
+    uploader = models.ForeignKey(User, on_delete=models.CASCADE)
+    file_path = models.CharField(max_length=200)
+    type = models.CharField(max_length=50)
+    size = models.IntegerField()
+    description = models.TextField()
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    is_private = models.BooleanField(default=False)

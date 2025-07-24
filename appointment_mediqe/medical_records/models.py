f58472881e3327abb1f6075db0ad3126bb3a6cfd
AmirMosafery <amirm4a112@gmail.com>
2025-07-24 03:50:20 -0700

    create medicalrecord model

--- appointment_mediqe/medical_records/models.py
+++ appointment_mediqe/medical_records/models.py
@@ -1,3 +1,22 @@
+import uuid
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+
+class MedicalRecord(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    STATUS_CHOICES = [
+        ('Open', 'Open'),
+        ('Closed', 'Closed'),
+        ('Archived', 'Archived'),
+    ]
+    patient = models.ForeignKey(User, on_delete=models.CASCADE)
+    doctor = models.ForeignKey(User, on_delete=models.CASCADE)
+    visit_reason = models.TextField(blank=True, null=True)
+    diagnosis = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='Open')
+    is_sensitive = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)

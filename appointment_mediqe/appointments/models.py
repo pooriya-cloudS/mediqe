4e3d682a16528c018ae6f7796fbf84b3af6b5a71
AmirMosafery <amirm4a112@gmail.com>
2025-07-26 20:06:52 +0330

    coding was cleaned

--- appointment_mediqe/appointments/models.py
+++ appointment_mediqe/appointments/models.py
@@ -3,16 +3,20 @@ from django.contrib.auth.models import User
 from django.db import models
 from django.conf import settings
 
+
 # Create your models here.
 
 class Schedule(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,blank=True, null=True ,related_name='doctor_schedule')
+    doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True,
+                               related_name='doctor_schedule')
     weekday = models.IntegerField()
     start_time = models.TimeField()
     end_time = models.TimeField()
     location = models.CharField(max_length=100)
     is_active = models.BooleanField(default=False)
+
+
 class Appointment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     STATUS_CHOICES = [
@@ -23,12 +27,14 @@ class Appointment(models.Model):
         ('NoShow', 'NoShow'),
     ]
 
-    doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE,blank=True, null=True,related_name='doctor_appointment')
-    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient_appointment',blank=True, null=True)
-    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE,blank=True, null=True)
+    doctor = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True,
+                               related_name='doctor_appointment')
+    patient = models.ForeignKey(User, on_delete=models.CASCADE, related_name='patient_appointment', blank=True,
+                                null=True)
+    schedule = models.ForeignKey(Schedule, on_delete=models.CASCADE, blank=True, null=True)
     appointment_time = models.TimeField()
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='Pending')
     created_by = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    cancelled_at =models.DateTimeField(auto_now=True)
+    cancelled_at = models.DateTimeField(auto_now=True)
     notes = models.TextField(blank=True)

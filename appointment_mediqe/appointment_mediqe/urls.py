041e053898532d881c11b7a7b511372bc7d53270
AmirMosafery <amirm4a112@gmail.com>
2025-07-28 00:41:12 +0330

    fix route and add api-auth for login in restframework template

--- appointment_mediqe/appointment_mediqe/urls.py
+++ appointment_mediqe/appointment_mediqe/urls.py
@@ -20,6 +20,7 @@ from django.urls import path,include
 from . import settings
 from django.conf.urls.static import static
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('',include('appointments.urls')),
+    path('api/v1/',include('appointments.api.v1.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

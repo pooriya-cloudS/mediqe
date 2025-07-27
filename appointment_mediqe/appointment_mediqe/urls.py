99b6cc3e20831a69f5ce0bdeb2072ab55d9f0c5b
AmirMosafery <amirm4a112@gmail.com>
2025-07-28 01:24:00 +0330

    fix more problem

--- appointment_mediqe/appointment_mediqe/urls.py
+++ appointment_mediqe/appointment_mediqe/urls.py
@@ -22,5 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
-    path('api/v1/',include('appointments.api.v1.urls')),
+    path('api/',include('appointments.api.v1.urls')),
 ]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

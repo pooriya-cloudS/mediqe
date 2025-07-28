2bdc0b252aca95a87376aea5216586f890f649bd
zeynab <zeynabb@yahoo.com>
2025-07-28 12:17:52 +0330

    models

--- appointment_mediqe/appointment_mediqe/settings.py
+++ appointment_mediqe/appointment_mediqe/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
@@ -19,16 +20,17 @@ load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY='django-insecure-w00mav)2r^pbi@tq=9*8f60zsl-kg-dogp^96'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [False]
 
 # Application definition
 
@@ -47,7 +49,8 @@ INSTALLED_APPS = [
     'chats.apps.ChatsConfig',
     'payments.apps.PaymentsConfig',
     'dashboards.apps.DashboardsConfig',
-    'organizations.apps.OrganizationsConfig'
+    'organizations.apps.OrganizationsConfig',
+    
 ]
 
 MIDDLEWARE = [
@@ -91,11 +94,18 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
-        "TEST": {
-            "NAME": os.environ.get("TEST_DB_NAME"),
-        }
+        # "TEST": {
+        #     "NAME": os.environ.get("mediqe_test_db"),
+        # }
     }
 }
+# # PostgreSQL Config
+# DB_ENGINE=django.db.backends.postgresql
+# DB_NAME=mediqe_test_db
+# DB_USER=mediqe
+# DB_PASSWORD=1@23456zZ#zza
+# DB_HOST=localhost
+# DB_PORT=5432
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators

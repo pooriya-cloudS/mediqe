1f6ffa88e564113c2276c1e65533f135e45ff26e
AmirMosafery <amirm4a112@gmail.com>
2025-08-06 12:23:13 +0330

    Update settings.py

--- appointment_mediqe/appointment_mediqe/settings.py
+++ appointment_mediqe/appointment_mediqe/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     "payments.apps.PaymentsConfig",
     "dashboards.apps.DashboardsConfig",
     "organizations.apps.OrganizationsConfig",
+]
 
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",

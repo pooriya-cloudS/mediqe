6dd308ee4793d2195f4fba54be1e8fbdd1540c64
zeynab <zeynabb@yahoo.com>
2025-08-08 20:40:58 +0330

    add view

--- appointment_mediqe/accounts/views.py
+++ appointment_mediqe/accounts/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import viewsets
+from .models import User, UserProfile
+from .serializers import UserSerializer, UserProfileSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows user profiles to be viewed or edited.
+    """
+    queryset = UserProfile.objects.all()
+    serializer_class = UserProfileSerializer
